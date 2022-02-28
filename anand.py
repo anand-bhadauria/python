8086018a840bcb795eed0d7b4f2fc1ad5040936b
anand-bhadauria <90555082+anand-bhadauria@users.noreply.github.com>
2022-02-28 16:43:50 +0530

    Add files via upload

---
+++ anand.py
@@ -0,0 +1,8 @@
+computer =" rock"
+user=input("enter what you chose rock paper or scissors")
+if user==" rock":
+    print("its a tie")
+elif user==" paper":
+    print("♩♩♩victory is what seems to be true♩♩♩♩♩♩♩♩")
+else:
+    print("boo you lost") 
